f6e76639a19f47713cacb3771594b3914ddb07da
Bruno Santos <brsantos197@gmail.com>
2024-12-01 18:00:42 -0300

    test 31

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const vars = args.find(arg => arg.startsWith('--vars='));
 const varsString = vars.split('=')[1];
 const varsObject = JSON.parse(varsString.replace(/\n/g, ''));
 
-console.log(varsObject);
+const varsArray = Object.values(varsObject).map((value) => `'${String(value).trim()}'`);
+
+console.log(`[${String(varsArray)}]`);

21a08f5250c7bdb864d4fcc0137b33f8fccad0cc
Bruno Santos <brsantos197@gmail.com>
2024-12-01 17:46:02 -0300

    test 30

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const args = process.argv.slice(2);
 
-console.log(args.join(' '));
+const vars = args.find(arg => arg.startsWith('--vars='));
+const varsString = vars.split('=')[1];
+const varsObject = JSON.parse(varsString.replace(/\n/g, ''));
+
+console.log(varsObject);

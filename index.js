7e7428af60a951186934b1e24702a6fdad16ef67
Bruno Santos <brsantos197@gmail.com>
2024-12-01 16:24:37 -0300

    test 17

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { getInput } from "@actions/core";
 
 const main = async () => {
   const vars = getInput("vars");
-  console.log(`vars ${typeof vars}!`);
+  console.log(`vars ${vars}!`);
 };
 
 main();

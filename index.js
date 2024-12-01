11931e9080462665620ecd90115d2459912ff884
Bruno Santos <brsantos197@gmail.com>
2024-12-01 16:42:07 -0300

    test 23

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { getInput } from "@actions/core";
 
 const main = async () => {
   const vars = getInput("vars");
-  console.log(`vars ${vars}!`);
+  console.log(`vars ${getInput("node_version")}!`);
 };
 
 main();

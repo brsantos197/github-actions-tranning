c22debc1383cb4673e8fac21a227dfc294eedf46
Bruno Santos <brsantos197@gmail.com>
2024-12-01 15:57:52 -0300

    test 12

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!', process.ENV);
+import { getInput } from "@actions/core";
+
+const main = async () => {
+  const vars = getInput("vars");
+  console.log(`vars ${vars}!`);
+};
+
+main();

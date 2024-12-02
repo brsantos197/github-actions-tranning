6cd98030f0ef0422f2b19bf9870f7d2cdcef89ff
Bruno Santos <brsantos197@gmail.com>
2024-12-02 00:51:22 -0300

    0

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import { writeFileSync } from "fs";
+import fs from 'fs';
+
+// Captura os argumentos de linha de comando
 const args = process.argv.slice(2);
 
+// Encontra a variável --vars
 const vars = args.find(arg => arg.startsWith('--vars='));
 const varsString = vars.split('=')[1];
+
+// Converte a string para objeto
 const varsObject = JSON.parse(varsString.replace(/\n/g, ''));
 
-const varsArray = Object.values(varsObject).map((value) => String(value).trim());
-writeFileSync('vars.txt', JSON.stringify(varsArray))
+// Converte os valores do objeto em um array de strings
+const varsArray = Object.values(varsObject).map(value => String(value).trim());
+
+// Agora define o output diretamente no arquivo $GITHUB_OUTPUT
+fs.appendFileSync(process.env.GITHUB_OUTPUT, `HOSTS=${JSON.stringify(varsArray)}\n`);

d466bea2b975590a9b9e5c1dd6d3600f35a24a57
Bruno Santos <brsantos197@gmail.com>
2024-12-02 00:32:51 -0300

    teste 6

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { writeFileSync } from "fs";
+import { execSync } from "child_process";
 const args = process.argv.slice(2);
 
 const vars = args.find(arg => arg.startsWith('--vars='));
 const varsString = vars.split('=')[1];
 const varsObject = JSON.parse(varsString.replace(/\n/g, ''));
 
-const varsArray = Object.values(varsObject).map((value) => `${String(value).trim()}`);
-
-console.log('console.log(varsArray)', varsArray)
-writeFileSync('vars.txt', JSON.stringify(varsArray))
+const hosts = Object.values(varsObject).map((value) => String(value).trim());
+execSync(`echo "HOSTS=${JSON.stringify(hosts)}" >> $GITHUB_OUTPUT`)

dd78dcc3abeff7798f90ee675d5df90ce731514c
Bruno Santos <brsantos197@gmail.com>
2024-12-02 00:42:28 -0300

    0

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const core = require('node:child_process').execSync;
-const args = require('minimist')(process.argv.slice(2));
+import { writeFileSync } from "fs";
+const args = process.argv.slice(2);
 
-// Obtém as variáveis de entrada
-const vars = JSON.parse(args.vars || '{}');
-console.log('Received vars:', vars);
+const vars = args.find(arg => arg.startsWith('--vars='));
+const varsString = vars.split('=')[1];
+const varsObject = JSON.parse(varsString.replace(/\n/g, ''));
 
-// Processa os dados (exemplo fictício)
-const hosts = ['192.168.1.1', '192.168.1.2', '192.168.1.3'];
-console.log('Generated hosts:', hosts);
-
-// Define o output
-core(`echo "HOSTS=${JSON.stringify(hosts)}" >> $GITHUB_OUTPUT`);
+const varsArray = Object.values(varsObject).map((value) => String(value).trim());
+writeFileSync('vars.txt', JSON.stringify(varsArray))
